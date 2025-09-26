b1bd6da2bf6585dbc350ffdbbdbedccf73506f88
xnoto <steven@makeitwork.cloud>
2025-09-26 02:50:18 -0600

    Changing settings

--- runner.tf
+++ runner.tf
@@ -75,7 +75,7 @@ resource "aap_host" "runner" {
   enabled      = true
   variables = jsonencode({
     ansible_host            = "${data.sops_file.secret_vars.data["runner_ip_addr"]}"
-    ansible_ssh_common_args = "-o ProxyCommand=\"ssh -W %h:%p -q ${data.sops_file.secret_vars.data["proxyhost"]}\""
+    ansible_ssh_common_args = "-J ${data.sops_file.secret_vars.data["proxyhost"]} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
   })
   depends_on = [libvirt_domain.runner]
 }
