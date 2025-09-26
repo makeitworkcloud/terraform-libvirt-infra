60a2e5b708a38d64a939dae6f701d83e8f2c6e9b
xnoto <steven@makeitwork.cloud>
2025-09-26 03:39:33 -0600

    Rebuilding

--- runner.tf
+++ runner.tf
@@ -1,3 +1,4 @@
+/*
 resource "libvirt_volume" "runner" {
   pool   = "default"
   source = "https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
@@ -84,3 +85,4 @@ resource "aap_job" "runner" {
   job_template_id = 9
   depends_on      = [aap_host.runner]
 }
+*/
