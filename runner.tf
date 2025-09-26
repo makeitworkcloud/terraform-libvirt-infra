ffc742c36527ade889efc9c4ef76edf4fe2c26d2
xnoto <steven@makeitwork.cloud>
2025-09-26 04:57:30 -0600

    Rebuilding for selinux.

--- runner.tf
+++ runner.tf
@@ -1,4 +1,3 @@
-/*
 resource "libvirt_volume" "runner" {
   pool   = "default"
   source = "https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
@@ -85,4 +84,3 @@ resource "aap_job" "runner" {
   job_template_id = 9
   depends_on      = [aap_host.runner]
 }
-*/
