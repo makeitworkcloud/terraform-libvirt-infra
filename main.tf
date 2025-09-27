902f11f9fa397b1384fbb31fc6e23a13a3f87036
xnoto <steven@makeitwork.cloud>
2025-09-27 11:11:08 -0600

    Default values

--- main.tf
+++ main.tf
@@ -6,9 +6,7 @@ module "runner" {
   source          = "./modules/mitw_domain"
   name            = "runner"
   description     = "GitHub Actions self-hosted runner"
-  vcpu            = 1
   memory          = 8192
-  boot_image_url  = "https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
   boot_image_name = "runner.qcow2"
   extra_volumes = [
     {
@@ -23,12 +21,9 @@ module "runner" {
   cloudinit_user_data_vars          = { ssh_authorized_key = data.sops_file.secret_vars.data["ssh_admin_pubkey"] }
   cloudinit_network_config_template = file("${path.module}/cloud-init/network_config.cfg")
   cloudinit_network_config_vars     = { private_ip_addr = data.sops_file.secret_vars.data["runner_ip_addr"] }
-  hostname                          = "runner"
   private_ip_addr                   = data.sops_file.secret_vars.data["runner_ip_addr"]
   proxyhost                         = data.sops_file.secret_vars.data["proxyhost"]
   enable_aap                        = true
-  aap_org_name                      = "Default"
-  aap_inventory_name                = "libvirt-infra"
   aap_job_template_name             = "configure_runner"
 }
 
@@ -36,22 +31,16 @@ module "torwww" {
   source                            = "./modules/mitw_domain"
   name                              = "torwww"
   description                       = "Tor hidden service web mirror"
-  vcpu                              = 1
   memory                            = 4096
-  boot_image_url                    = "https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
   boot_image_name                   = "torwww.qcow2"
-  extra_volumes                     = []
   cloudinit_meta_data_template      = file("${path.module}/cloud-init/meta_data.cfg")
   cloudinit_meta_data_vars          = { hostname = "torwww" }
   cloudinit_user_data_template      = file("${path.module}/cloud-init/torwww/cloud_init.cfg")
   cloudinit_user_data_vars          = { ssh_authorized_key = data.sops_file.secret_vars.data["ssh_admin_pubkey"] }
   cloudinit_network_config_template = file("${path.module}/cloud-init/network_config.cfg")
   cloudinit_network_config_vars     = { private_ip_addr = data.sops_file.secret_vars.data["torwww_ip_addr"] }
-  hostname                          = "torwww"
   private_ip_addr                   = data.sops_file.secret_vars.data["torwww_ip_addr"]
   proxyhost                         = data.sops_file.secret_vars.data["proxyhost"]
   enable_aap                        = true
-  aap_org_name                      = "Default"
-  aap_inventory_name                = "libvirt-infra"
   aap_job_template_name             = "configure_torwww"
 }
--- modules/mitw_domain/vars.tf
+++ modules/mitw_domain/vars.tf
@@ -6,21 +6,25 @@ variable "name" {
 variable "description" {
   description = "Description for the libvirt domain (virtual machine)."
   type        = string
+  default     = ""
 }
 
 variable "vcpu" {
   description = "Number of virtual CPUs to assign to the domain."
   type        = number
+  default     = 1
 }
 
 variable "memory" {
   description = "Amount of memory (in MB) to assign to the domain."
   type        = number
+  default     = 2048
 }
 
 variable "boot_image_url" {
   description = "URL for the base QCOW2 image used as the boot disk."
   type        = string
+  default     = "https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
 }
 
 variable "boot_image_name" {
@@ -81,11 +85,6 @@ variable "cloudinit_network_config_vars" {
   type        = map(string)
 }
 
-variable "hostname" {
-  description = "Hostname for the VM. Used for naming and cloud-init."
-  type        = string
-}
-
 variable "private_ip_addr" {
   description = "Private IP address to assign to the VM (used for network config and inventory)."
   type        = string
@@ -99,17 +98,19 @@ variable "proxyhost" {
 variable "enable_aap" {
   description = "Whether to provision Ansible Automation Platform (AAP) resources for this domain."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "aap_org_name" {
   description = "Name of the Ansible Automation Platform (AAP) organization."
   type        = string
+  default     = "Default"
 }
 
 variable "aap_inventory_name" {
   description = "Name of the AAP inventory to use."
   type        = string
+  default     = "libvirt-infra"
 }
 
 variable "aap_job_template_name" {
